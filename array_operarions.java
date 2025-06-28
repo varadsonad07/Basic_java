b77c1e81471ebc1b5be2b964fa2db640563ab7fa
VARAD BHAGWAN SONAD <vbsonad07@gmail.com>
2025-06-28 20:45:12 +0530

    Add files via upload
    using array , we can create some operations in different different classes

---
+++ array_operarions.java
@@ -0,0 +1,23 @@
+package com.sunbeam;
+import java.util.Scanner;
+
+public class array_operarions {
+
+	public static void main(String[] args) {
+		
+	 human[ ] arr = new human[4];
+
+	 arr[0] = new human(12 , 30 , 45);
+	 arr[1] = new human(12 , 30 , 45);
+	 arr[2] = new human(12 , 30 , 45);
+	 arr[3] = new human(12 , 30 , 45);
+ 
+	
+	
+	for(int i = 0 ; i < arr.length ; i++) {
+
+		arr[i].display();
+	}
+	
+	}
+}
